47adb554fc9857c26651c7272d349ddf7c90af02
darkzard05 <darkzard05@gmail.com>
2023-01-11 18:40:40 +0900

    Refactor models

--- nn_model.py
+++ nn_model.py
@@ -7,25 +7,26 @@ class appnp(torch.nn.Module):
     def __init__(self, dataset, trial):
         super().__init__()
         self.num_layers = trial.suggest_int('num_layers', 1, 3)
-        self.K = trial.suggest_int('K', 1, 200)
-        self.alpha = trial.suggest_float('alpha', 0, 0.2)
+        self.K = trial.suggest_int('K', 5, 200)
+        self.alpha = trial.suggest_float('alpha', 0.0, 0.5)
         
         self.layers_list = nn.ModuleList()
         self.dropout_list = []
         self.units_list = []
         
         if self.num_layers >= 2:
-            for epoch in range(self.num_layers-1):
-                self.units_list.append(trial.suggest_int(f'n_units_{epoch}', 5, 200))
+            for epoch in range(1, self.num_layers):
+                self.units_list.append(trial.suggest_categorical(f'n_units_{epoch}',
+                                                                 [4, 8, 16, 32, 64, 128, 256, 512]))
         
         self.in_features = dataset[0].num_features
         
-        for epoch in range(self.num_layers):
-            self.dropout_list.append(trial.suggest_float(f'dropout_{epoch}', 0.1, 0.9))
-            if epoch == self.num_layers-1:
+        for epoch in range(1, self.num_layers+1):
+            self.dropout_list.append(trial.suggest_float(f'dropout_{epoch}', 0.0, 1.0))
+            if epoch == self.num_layers:
                 out_features = dataset.num_classes
             else:
-                out_features = self.units_list[epoch]
+                out_features = self.units_list[epoch-1]
             self.layers_list.append(Linear(self.in_features, out_features))
             self.in_features = out_features
         self.prop = APPNP(K=self.K, alpha=self.alpha)
@@ -34,8 +35,8 @@ class appnp(torch.nn.Module):
         for layer in self.layers_list:
             layer.reset_parameters()
         
-    def forward(self, d):
-        x, edge_index = d.x, d.edge_index
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
         for i, layer in enumerate(self.layers_list):
             x = F.dropout(x, p=self.dropout_list[i], training=self.training)
             x = layer(x).relu() 
@@ -45,37 +46,22 @@ class appnp(torch.nn.Module):
 class splineconv(torch.nn.Module):
     def __init__(self, dataset, trial):
         super().__init__()
-        self.num_layers = trial.suggest_int('num_layers', 1, 3)
-        
-        self.layers_list = nn.ModuleList()
-        self.dropout_list = []
-        self.units_list = []
-        
-        if self.num_layers >= 2:
-            for n in range(self.num_layers -1):
-                self.units_list.append(trial.suggest_int(f'n_units_{n}', 5, 200))
-                
         self.in_features = dataset[0].num_features
+        self.out_features = dataset.num_classes
+        self.dropout_1 = trial.suggest_float('dropout_1', 0.0, 1.0)
+        self.dropout_2 = trial.suggest_float('dropout_2', 0.0, 1.0)
+        self.n_units = trial.suggest_categorical('n_units', [4, 8, 16, 32, 64, 128, 256, 512, 1024])
+        self.conv_1 = SplineConv(self.in_features, self.n_units, dim=1, kernel_size=2)
+        self.conv_2 = SplineConv(self.n_units, self.out_features, dim=1, kernel_size=2)
         
-        for epoch in range(self.num_layers):
-            self.dropout_list.append(trial.suggest_float(f'dropout_{epoch}', 0.1, 0.9))
-            if epoch == self.num_layers-1:
-                out_features = dataset.num_classes
-            else:
-                out_features = self.units_list[epoch]
-            self.layers_list.append(SplineConv(self.in_features,
-                                               out_features,
-                                               dim = 1,
-                                               kernel_size = trial.suggest_int('kernel_size', 1, 6)))
-            self.in_features = out_features
-
     def reset_parameters(self):
-        for layer in self.layers_list:
-            layer.reset_parameters()
+        self.conv_1.reset_parameters()
+        self.conv_2.reset_parameters()
         
     def forward(self, data):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
-        for i, layer in enumerate(self.layers_list):
-            x = F.dropout(x, p=self.dropout_list[i], training=self.training)
-            x = layer(x, edge_index, edge_attr).relu()
+        x = F.dropout(x, p=self.dropout_1, training=self.training)
+        x = self.conv_1(x, edge_index, edge_attr).relu()
+        x = F.dropout(x, p=self.dropout_2, training=self.training)
+        x = self.conv_2(x, edge_index, edge_attr)
         return F.log_softmax(x, dim=1)
