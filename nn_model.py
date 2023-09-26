c9efb38097331c901bbfe9df786b95cbcf094b85
darkzard05 <darkzard05@gmail.com>
2023-09-26 14:44:26 +0900

    Refactor: Add build_model_list

--- nn_model.py
+++ nn_model.py
@@ -7,10 +7,23 @@ from torch_geometric.typing import Adj, OptTensor
 
 
 ACTIVATION_FUNCTIONS = {
-    'relu': torch.nn.ReLU,
-    'leakyrelu': torch.nn.LeakyReLU,
-    'elu': torch.nn.ELU
+    'relu': torch.nn.ReLU(),
+    'leakyrelu': torch.nn.LeakyReLU(),
+    'elu': torch.nn.ELU()
 }
+
+
+def build_model_list(model, in_channels, out_channels, n_units, num_layers,
+                     heads=1, multi_factor=1, **kwargs):
+    model_list = ModuleList()
+    for layer in range(1, num_layers+1):
+        if layer == num_layers:
+            current = out_channels
+        else:
+            current = n_units * multi_factor
+        model_list.append(model(in_channels, current, **kwargs))
+        in_channels = current
+    return model_list
 
 
 class APPNPModel(torch.nn.Module):
@@ -31,19 +44,10 @@ class APPNPModel(torch.nn.Module):
         self.K = K
         self.alpha = alpha
         self.dropout = dropout
-        self.activation = ACTIVATION_FUNCTIONS[activation]()
+        self.activation = ACTIVATION_FUNCTIONS[activation]
         self.num_layers = num_layers
-        self.model_list = ModuleList()
-        if self.num_layers == 1:
-            self.model_list = ModuleList([Linear(self.in_channels, self.out_channels)])
-        elif self.num_layers == 2:
-            self.model_list = ModuleList([Linear(self.in_channels, self.n_units),
-                                          Linear(self.n_units, self.out_channels)])
-        else:
-            self.model_list.append(Linear(self.in_channels, self.n_units))
-            for _ in range(2, self.num_layers):
-                self.model_list.append(Linear(self.n_units, self.n_units))
-            self.model_list.append(Linear(self.n_units, self.out_channels))
+        self.model_list = build_model_list(Linear, self.in_channels, self.out_channels,
+                                           self.n_units, self.num_layers)
         self.prop = APPNP(K=self.K, alpha=self.alpha)
         self.reset_parameters()
         
@@ -80,29 +84,11 @@ class SplineconvModel(torch.nn.Module):
         self.kernel_size = kernel_size
         self.n_units = n_units
         self.dropout = dropout
-        self.activation = ACTIVATION_FUNCTIONS[activation]()
+        self.activation = ACTIVATION_FUNCTIONS[activation]
         self.num_layers = num_layers
-        self.model_list = ModuleList()
-        if self.num_layers == 1:
-            self.model_list = ModuleList([SplineConv(self.in_channels, self.out_channels,
-                                                  dim=1, kernel_size=self.kernel_size)])
-        elif self.num_layers == 2:
-            self.model_list = ModuleList([SplineConv(self.in_channels, self.n_units,
-                                                  dim=1, kernel_size=self.kernel_size),
-                                          SplineConv(self.n_units, self.out_channels,
-                                                  dim=1, kernel_size=self.kernel_size)])
-        else:
-            self.model_list.append(SplineConv(self.in_channels, self.n_units,
-                                           dim=1, kernel_size=self.kernel_size))
-            for _ in range(2, self.num_layers):
-                self.model_list.append(SplineConv(self.n_units, self.n_units,
-                                               dim=1, kernel_size=self.kernel_size))
-            self.model_list.append(SplineConv(self.n_units, self.out_channels,
-                                           dim=1, kernel_size=self.kernel_size))
-        # self.conv_1 = SplineConv(self.in_channels, self.n_units,
-        #                          dim=1, kernel_size=self.kernel_size)
-        # self.conv_2 = SplineConv(self.n_units, self.out_channels,
-        #                          dim=1, kernel_size=self.kernel_size)
+        self.model_list = build_model_list(SplineConv, self.in_channels, self.out_channels,
+                                           self.n_units, self.num_layers,
+                                           dim=1, kernel_size=self.kernel_size)
         self.reset_parameters()
         
     def reset_parameters(self):
@@ -122,7 +108,8 @@ class SplineconvModel(torch.nn.Module):
     
 class GATModel(torch.nn.Module):
     def __init__(self,
-                 in_channels: int, out_channels: int,
+                 in_channels: int,
+                 out_channels: int,
                  n_units: int = 32,
                  heads: int = 8,
                  dropout: float = 0.5,
@@ -135,26 +122,11 @@ class GATModel(torch.nn.Module):
         self.n_units = n_units
         self.heads = heads
         self.dropout = dropout
+        self.activation = ACTIVATION_FUNCTIONS[activation]
         self.num_layers = num_layers
-        self.activation = ACTIVATION_FUNCTIONS[activation]()
-        self.model_list = ModuleList()
-        if self.num_layers == 1:
-            self.model_list = ModuleList([GATConv(self.in_channels, self.out_channels, dropout=self.dropout)])
-        elif self.num_layers == 2:
-            self.model_list = ModuleList([GATConv(self.in_channels, self.n_units, heads=self.heads,
-                                                  dropout=self.dropout),
-                                          GATConv(self.n_units * self.heads, self.out_channels,
-                                                  dropout=self.dropout)])
-        else:
-            self.model_list.append(GATConv(self.in_channels, self.n_units, heads=self.heads,
-                                           dropout=self.dropout))
-            temp = self.n_units * self.heads
-            for _ in range(2, self.num_layers):
-                self.model_list.append(GATConv(temp, temp, heads=self.heads,
-                                               dropout=self.dropout))
-                temp *= self.heads
-            self.model_list.append(GATConv(temp, self.out_channels,
-                                           dropout=self.dropout))
+        self.model_list = build_model_list(GATConv, self.in_channels, self.out_channels,
+                                           self.n_units, self.num_layers, heads=self.heads,
+                                           dropout=self.dropout, multi_factor=self.heads)
         self.reset_parameters()
         
     def reset_parameters(self):
@@ -165,10 +137,8 @@ class GATModel(torch.nn.Module):
                 x: Tensor,
                 edge_index: Adj,
                 edge_attr : OptTensor = None) -> Tensor:
-        for gatconv in self.model_list:
+        for i, gatconv in enumerate(self.model_list):
             x = gatconv(x, edge_index, edge_attr)
-            x = self.activation(x)
-        # x = self.conv_1(x, edge_index, edge_attr)
-        # x = self.activation(x)
-        # x = self.conv_2(x, edge_index, edge_attr)
+            if i != len(self.model_list)-1:
+                x = self.activation(x)
         return x
