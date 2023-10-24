d841ad2bdf4b03f8e25c3c4c90de6513b78844fe
darkzard05 <darkzard05@gmail.com>
2023-10-24 22:40:58 +0900

    Feat: add load_train_loader in load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -1,13 +1,16 @@
-from torch_geometric.loader import DataLoader
+from torch_geometric.loader import NeighborLoader
 from torch_geometric.datasets import Planetoid, Reddit
 import torch_geometric.transforms as T
 
 def load_dataset(path, name,
                  split='public',
-                 batch_size=64,
                  transform=T.TargetIndegree()):
     if name == 'Reddit':
-        dataset = Reddit(root=path, transform=transform)
-        return DataLoader(dataset, batch_size=batch_size)
-    
+        return Reddit(root=path, transform=transform)
+        
     return Planetoid(root=path, name=name, split=split, transform=transform)
+
+def load_train_loader(data, num_neighbors, batch_size):
+    train_loader = NeighborLoader(data=data, num_neighbors=num_neighbors,
+                                  input_nodes=data.train_mask, batch_size=batch_size)
+    return train_loader
