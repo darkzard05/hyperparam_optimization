4cdb6e3fe14609cd1ff560ace5ef8e2c329fe227
darkzard05 <darkzard05@gmail.com>
2023-11-01 18:02:29 +0900

    Refactor: rename load_ and get_

--- load_dataset.py
+++ load_dataset.py
@@ -2,16 +2,15 @@ from torch_geometric.loader import NeighborLoader
 from torch_geometric.datasets import Planetoid, Reddit
 import torch_geometric.transforms as T
 
-def load_dataset(path, name,
-                 split='public',
-                 transform=T.TargetIndegree()):
+def get_dataset(path, name, split='public', transform=T.TargetIndegree()):
     if name == 'Reddit':
         return Reddit(root=path, transform=transform)
         
     return Planetoid(root=path, name=name, split=split, transform=transform)
 
-def load_train_loader(data, num_neighbors, batch_size, num_workers=4):
-    kwargs = {'num_neighbors': num_neighbors, 'batch_size': batch_size,
-              'pin_memory': True, 'num_workers': num_workers, 'shuffle': True}
-    train_loader = NeighborLoader(data=data, input_nodes=data.train_mask, **kwargs)
+def get_train_loader(data, num_neighbors, batch_size, num_workers=4):
+    kwargs = {'pin_memory': True, 'shuffle': True}
+    train_loader = NeighborLoader(data=data, input_nodes=data.train_mask,
+                                  num_neighbors=num_neighbors, batch_size=batch_size,
+                                  num_workers=num_workers, **kwargs)
     return train_loader
