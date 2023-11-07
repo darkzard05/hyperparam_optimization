25373837bcd92731cf3fbfd6b05c02205df3486a
darkzard05 <darkzard05@gmail.com>
2023-11-07 18:50:03 +0900

    refactor: delete param split

--- load_dataset.py
+++ load_dataset.py
@@ -2,11 +2,11 @@ from torch_geometric.loader import NeighborLoader
 from torch_geometric.datasets import Planetoid, Reddit
 import torch_geometric.transforms as T
 
-def get_dataset(path, name, split='public', transform=T.TargetIndegree()):
+def get_dataset(path, name, transform=T.TargetIndegree()):
     if name == 'Reddit':
         return Reddit(root=path, transform=transform)
         
-    return Planetoid(root=path, name=name, split=split, transform=transform)
+    return Planetoid(root=path, name=name, transform=transform)
 
 def get_train_loader(data, num_neighbors, batch_size, num_workers=4):
     kwargs = {'pin_memory': True, 'shuffle': True}
