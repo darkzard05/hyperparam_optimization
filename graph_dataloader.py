de62b4fad7f3c268972f790cc5fefdfcf8fe1ddf
darkzard05 <darkzard05@gmail.com>
2023-11-14 17:33:42 +0900

    refactor: set num_workers to 0

--- graph_dataloader.py
+++ graph_dataloader.py
@@ -21,12 +21,11 @@ def get_dataset(path, name, transform=T.TargetIndegree()):
     return Planetoid(root=path, name=name, transform=transform)
 
 
-def get_train_loader(data, num_neighbors, batch_size, num_workers=4):
-    kwargs = {'pin_memory': True, 'shuffle': True}
+def get_train_loader(data, num_neighbors, batch_size):
+    kwargs = {'num_workers': 0, 'pin_memory': True, 'shuffle': True}
     train_loader = NeighborLoader(data=data,
                                   num_neighbors=num_neighbors,
                                   input_nodes=data.train_mask,
                                   batch_size=batch_size,
-                                  num_workers=num_workers,
                                   **kwargs)
     return train_loader
