420354e2fdbbaa1c28367645adeb8fc47c302c90
darkzard05 <darkzard05@gmail.com>
2023-12-10 20:52:39 +0900

    refactor: simplified data loader integration

--- graph_dataloader.py
+++ graph_dataloader.py
@@ -7,10 +7,8 @@ import torch_geometric.transforms as T
 
 
 def preprocess_data(data) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-    x = (data.x - data.x.mean()) / data.x.std()
-    edge_index = data.edge_index
-    edge_attr = data.edge_attr
-    return x, edge_index, edge_attr
+    x = (data.x - data.x.mean(dim=0, keepdim=True)) / data.x.std(dim=0, keepdim=True)
+    return x, data.edge_index, data.edge_attr
 
 
 def get_dataset(path, name, transform=T.TargetIndegree()):
@@ -21,22 +19,19 @@ def get_dataset(path, name, transform=T.TargetIndegree()):
 
 
 def get_dataloader(data, num_neighbors, batch_size, num_workers):
-    kwargs = {'num_workers': num_workers, 'pin_memory': True,
-              'persistent_workers': True if num_workers > 0 else False,
-              'shuffle': True}
-    train_loader = NeighborLoader(data=data,
-                                  num_neighbors=num_neighbors,
-                                  input_nodes=data.train_mask,
-                                  batch_size=batch_size,
-                                  **kwargs)
-    val_loader = NeighborLoader(data=data,
-                                num_neighbors=num_neighbors,
-                                input_nodes=data.val_mask,
-                                batch_size=batch_size,
-                                **kwargs)
-    test_loader = NeighborLoader(data=data,
-                                 num_neighbors=num_neighbors,
-                                 input_nodes=data.test_mask,
-                                 batch_size=batch_size,
-                                 **kwargs)
+    loader_kwargs = {'num_workers': num_workers, 'pin_memory': True,
+                     'persistent_workers': True if num_workers > 0 else False,
+                     'shuffle': True}
+    
+    def create_loader(mask):
+        return NeighborLoader(data=data,
+                              num_neighbors=num_neighbors,
+                              input_nodes=mask,
+                              batch_size=batch_size,
+                              **loader_kwargs)
+    
+    train_loader = create_loader(data.train_mask)
+    val_loader = create_loader(data.val_mask)
+    test_loader = create_loader(data.test_mask)
+    
     return train_loader, val_loader, test_loader
